1902c8aa0f9fbe1f61bc2e6f035bbc2f67c44b46
MD-Moniruzzaman-Arafat <arafatmonir091@gmail.com>
2025-09-09 11:15:22 +0600

    companiesLogo section component design

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import CompaniesLogo from './components/companiesLogo/CompaniesLogo'
 import Header from './components/header/Header'
 import Hero from './components/hero/Hero'
 
@@ -7,6 +8,7 @@ function App() {
     <>
       <Header />
       <Hero />
+      <CompaniesLogo />
     </>
   )
 }
